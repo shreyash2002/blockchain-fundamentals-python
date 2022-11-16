7753f134b91651a73e22e8c419ee3a754e9b2359
Shreyash Chauhan <chauhanmanthan04@gmail.com>
2022-11-16 19:06:09 +0530

    program complete

--- simple-blockchain.py
+++ simple-blockchain.py
@@ -3,3 +3,13 @@ from hashlib import sha256
 import json
 
 import time 
+
+class Chain:
+
+    def __init__(self):
+
+        self.blockchain = []
+
+        self.pending = []
+
+        self.add_block(prevhash="Genesis", proof=123)
